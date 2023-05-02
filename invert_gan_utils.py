f2ee7ffa1411d448c0542694b824b0b041f6c450
Cata400 <cata0310@gmail.com>
2023-05-03 01:36:23 +0300

    added complete GAN inversion and gitignore file

--- invert_gan_utils.py
+++ invert_gan_utils.py
@@ -1,7 +1,48 @@
-import torchvision
+import matplotlib.pyplot as plt
+import torch
+
+from argparse import Namespace
+from PIL import Image
+
+from restyle.configs import data_configs
+from restyle.models.e4e import e4e
+from restyle.utils.inference_utils import run_on_batch, get_average_image
+from restyle.utils.common import tensor2im
 
 
-def get_image(image_path, opts):
-    image = torchvision.io.read_image(image_path)
-    image = image.to(opts.device).unsqueeze(0)
-    return image
+def invert_image(image, invert_gan_path):
+    # Read checkpoint
+    checkpoint = torch.load(invert_gan_path, map_location='cpu')
+    invert_gan_state_dict = checkpoint['state_dict']
+    invert_gan_opts = checkpoint['opts']
+    
+    # Override some options
+    invert_gan_opts['checkpoint_path'] = invert_gan_path
+    invert_gan_opts['batch_size'] = 1
+    invert_gan_opts['n_iters_per_batch'] = 1
+    invert_gan_opts = Namespace(**invert_gan_opts)
+    
+    # Load Invert GAN model
+    invert_gan = e4e(invert_gan_opts)
+    invert_gan.load_state_dict(invert_gan_state_dict)
+    invert_gan.eval()
+    invert_gan.to(invert_gan_opts.device)
+    
+    # Prepare image
+    dataset_args = data_configs.DATASETS[invert_gan_opts.dataset_type]
+    transforms_dict = dataset_args['transforms'](invert_gan_opts).get_transforms()
+    image = transforms_dict['transform_inference'](image)
+    image = image.unsqueeze(0)
+    
+    average_image = get_average_image(invert_gan, invert_gan_opts)
+    
+    resize_amount = (invert_gan_opts.output_size, invert_gan_opts.output_size)
+        
+    with torch.no_grad():
+        input_cuda = image.cuda().float()
+        result, result_latent = run_on_batch(input_cuda, invert_gan, invert_gan_opts, average_image)
+                
+        result = tensor2im(result[0][0])
+        result.resize(resize_amount)
+
+    return result, result_latent[0][0]
--- main.py
+++ main.py
@@ -1,48 +1,36 @@
 import os
-import matplotlib.pyplot as plt
-import torch
-import torchvision
 
-from restyle.models.e4e import e4e
+import numpy as np
+from PIL import Image
 
-from invert_gan_utils import *
-
-from argparse import Namespace
-from configs import data_configs
+from invert_gan_utils import invert_image
 
 
 if __name__ == '__main__':
-    IMAGE_PATH = os.path.join('..', 'Images', 'Input', 'Lenna.png')
-    INVERT_GAN_PATH = os.path.join('..', 'Models', 'restyle_gan.pt')
+    REFERENCE_IMAGE_NAME = 'Lenna'
+    REFERENCE_IMAGE_EXT = '.png'
+    REFERENCE_IMAGE_PATH = os.path.join('..', 'Images', 'References', REFERENCE_IMAGE_NAME + REFERENCE_IMAGE_EXT)
+    REFERENCE_INVERT_GAN_PATH = os.path.join('..', 'Models', 'restyle_gan.pt')
+    
+    REFERENCE_IMAGE_TRAINING_SET_PATH = os.path.join('..', 'Images', 'Training set', REFERENCE_IMAGE_NAME)
+    
+    INPUT_IMAGE_NAME = 'Lenna'
+    INPUT_IMAGE_EXT = '.png'
+    INPUT_IMAGE_PATH = os.path.join('..', 'Images', 'Input', INPUT_IMAGE_NAME + INPUT_IMAGE_EXT)
 
-    image = torchvision.io.read_image(IMAGE_PATH)
-
-    plt.figure(), plt.imshow(image.permute(1, 2, 0).numpy()), plt.show()
-
-    checkpoint = torch.load(INVERT_GAN_PATH)
-    invert_gan_state_dict = checkpoint['state_dict']
-    invert_gan_opts = checkpoint['opts']
-    invert_gan_opts = Namespace(**invert_gan_opts)
-    
-    invert_gan = e4e(invert_gan_opts)
-    invert_gan.load_state_dict(invert_gan_state_dict)
-    invert_gan.eval()
-    invert_gan.to(invert_gan_opts.device)
-    
-    dataset_args = data_configs.DATASETS[invert_gan_opts.dataset_type]
-    transforms_dict = dataset_args['transforms'](invert_gan_opts).get_transforms()
-    
-    print(dataset_args)
-    
-    print("-------------")
-    
-    print(transforms_dict)
+    OUTPUT_IMAGE_NAME = 'Lenna'
+    OUTPUT_IMAGE_PATH = os.path.join('..', 'Images', 'OUTPUT', OUTPUT_IMAGE_NAME + INPUT_IMAGE_EXT)
     
 
-    print(invert_gan_state_dict)
-    print("-------------")
-    print(invert_gan_opts)
+    ### Step 1: GAN Invert
+    # Read reference image
+    print('Inverting reference image...')
+    reference_image = Image.open(REFERENCE_IMAGE_PATH).convert('RGB')
 
+    result, result_latent = invert_image(reference_image, REFERENCE_INVERT_GAN_PATH)
 
-
-
+    result.save(os.path.join('..', 'Images', 'Inverted', REFERENCE_IMAGE_NAME + '_invert_gan_' + REFERENCE_IMAGE_EXT))
+    np.save(os.path.join('..', 'Images', 'Inverted latents', REFERENCE_IMAGE_NAME + '_invert_latent_code.npy'), result_latent)
+    
+    
+    ### Step 2: Create training set
